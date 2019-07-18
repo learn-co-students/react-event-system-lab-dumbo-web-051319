c31d5aabc2b763af72e0b8aec0b82d4c1a210cfc
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-17 23:30:33 -0400

    practiced using events

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  handleEyes = () => {
+    console.log("Good!");
+  }
+
+  handleNoEyes = () => {
+    console.log("Hey! Eyes on me!");
+  }
+
+  render() {
+    return (
+      <div>
+        <button onFocus={this.handleEyes} onBlur={this.handleNoEyes} >BUTTON</button>
+      </div>
+    )
+  }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  handleKey = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return (
+      <div>
+        <input onKeyUp={this.handleKey} type="password" />
+      </div>
+    )
+  }
+
+}
